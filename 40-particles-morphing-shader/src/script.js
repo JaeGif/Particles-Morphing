18ca74673744cf7cd6e51cf6a2a9c97a5366bcb6
Jae <giffordjacob0@gmail.com>
2024-06-11 12:35:19 -0700

    add simplex noise

--- 40-particles-morphing-shader/src/script.js
+++ 40-particles-morphing-shader/src/script.js
@@ -1,122 +1,187 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
-import GUI from 'lil-gui'
-import gsap from 'gsap'
-import particlesVertexShader from './shaders/particles/vertex.glsl'
-import particlesFragmentShader from './shaders/particles/fragment.glsl'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import GUI from 'lil-gui';
+import gsap from 'gsap';
+import particlesVertexShader from './shaders/particles/vertex.glsl';
+import particlesFragmentShader from './shaders/particles/fragment.glsl';
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI({ width: 340 })
-const debugObject = {}
+const gui = new GUI({ width: 340 });
+const debugObject = {};
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Loaders
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('./draco/')
-const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('./draco/');
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    pixelRatio: Math.min(window.devicePixelRatio, 2)
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
+  if (particles)
     // Materials
-    particles.material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    );
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(sizes.pixelRatio);
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 8 * 2)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 8 * 2);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
-})
+  canvas: canvas,
+  antialias: true,
+});
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(sizes.pixelRatio)
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
 
-debugObject.clearColor = '#160920'
-gui.addColor(debugObject, 'clearColor').onChange(() => { renderer.setClearColor(debugObject.clearColor) })
-renderer.setClearColor(debugObject.clearColor)
+debugObject.clearColor = '#160920';
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+  renderer.setClearColor(debugObject.clearColor);
+});
+renderer.setClearColor(debugObject.clearColor);
+let particles = null;
 
-/**
- * Particles
- */
-const particles = {}
+// Load models
+gltfLoader.load('./models.glb', (gltf) => {
+  /**
+   * Particles
+   */
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3)
+  particles = {};
+  particles.maxCount = 0; // size of the biggest gltf mesh count
 
-// Material
-particles.material = new THREE.ShaderMaterial({
+  // positions
+  const positions = gltf.scene.children.map(
+    (child) => child.geometry.attributes.position
+  );
+  for (const position of positions) {
+    if (position.count > particles.maxCount)
+      particles.maxCount = position.count;
+  }
+
+  // update positions arrays to have same vals as max count
+  particles.positions = [];
+  for (const position of positions) {
+    const originalArray = position.array;
+    if (originalArray.length === particles.maxCount) continue;
+    const newArray = new Float32Array(particles.maxCount * 3);
+
+    for (let i = 0; i < particles.maxCount; i++) {
+      const i3 = i * 3;
+
+      if (i3 < originalArray.length) {
+        newArray[i3 + 0] = originalArray[i3 + 0];
+        newArray[i3 + 1] = originalArray[i3 + 1];
+        newArray[i3 + 2] = originalArray[i3 + 2];
+      } else {
+        const randomIndex = Math.floor(position.count * Math.random()) * 3;
+
+        newArray[i3 + 0] = originalArray[randomIndex + 0];
+        newArray[i3 + 1] = originalArray[randomIndex + 1];
+        newArray[i3 + 2] = originalArray[randomIndex + 2];
+      }
+    }
+    particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3));
+  }
+  // Geometry
+  particles.geometry = new THREE.BufferGeometry();
+  particles.geometry.setAttribute('position', particles.positions[1]);
+  particles.geometry.setAttribute('aPositionTarget', particles.positions[3]);
+
+  // Material
+  particles.material = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
-    uniforms:
-    {
-        uSize: new THREE.Uniform(0.4),
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio))
-    }
-})
+    uniforms: {
+      uSize: new THREE.Uniform(0.2),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+      uProgress: new THREE.Uniform(0),
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+  });
 
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material)
-scene.add(particles.points)
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material);
+  scene.add(particles.points);
+
+  // tweaks
+  gui
+    .add(particles.material.uniforms.uProgress, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uProgress');
+});
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const tick = () => {
+  // Update controls
+  controls.update();
 
-    // Render normal scene
-    renderer.render(scene, camera)
+  // Render normal scene
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
